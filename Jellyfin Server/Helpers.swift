adaea2356a434a236cc2de5e42f529cd103bc1bd
gnattu <gnattuoc@me.com>
2024-09-08 22:28:26 +0800

    Use server networking settings to open url

--- Jellyfin Server/Helpers.swift
+++ Jellyfin Server/Helpers.swift
@@ -18,3 +18,47 @@ func directoryExists(path: String) -> Bool {
     var isDirectory: ObjCBool = false
     return FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory)
 }
+
+func getJellyfinNetworkConfig() -> (port: String, proto: String, subPath:String) {
+    let configFile: URL = directoryExists(path: localShareJellyfinFolder.path)
+        ? localShareJellyfinFolder.appendingPathComponent("config/network.xml")
+        : applicationSupportJellyfinFolder.appendingPathComponent("config/network.xml")
+    var httpPort = "8096"
+    var httpsPort = "8920"
+    var requireHttps = false
+    var enableHttps = false
+    var subPath = ""
+    do {
+        let config = try XMLDocument(contentsOf: configFile)
+        try config.validate()
+        if let rootElement = config.rootElement() {
+            if let internalHttpPort = rootElement.elements(forName: "InternalHttpPort").first?.stringValue {
+                httpPort = internalHttpPort
+            }
+            
+            if let internalHttpsPort = rootElement.elements(forName: "InternalHttpsPort").first?.stringValue {
+                httpsPort = internalHttpsPort
+            }
+            
+            if let enableHttpsString = rootElement.elements(forName: "EnableHttps").first?.stringValue {
+                enableHttps = (enableHttpsString as NSString).boolValue
+            }
+            
+            if let requireHttpsString = rootElement.elements(forName: "RequireHttps").first?.stringValue {
+                requireHttps = (requireHttpsString as NSString).boolValue
+            }
+            
+            if let baseUrl = rootElement.elements(forName: "BaseUrl").first?.stringValue, !baseUrl.isEmpty {
+                subPath = baseUrl
+            }
+        }
+    } catch {
+        print("Jellyfin Server config is invalid, using default values.")
+    }
+    
+    if requireHttps && enableHttps {
+        return (httpsPort, "https", subPath)
+    } else {
+        return (httpPort, "http", subPath)
+    }
+}
